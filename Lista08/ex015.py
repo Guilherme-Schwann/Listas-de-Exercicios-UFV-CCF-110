4d4ea96cc0386b901306ba533a5218f0ffc4aa2a
Guilherme-Schwann <guilhermeawilke@hotmail.com>
2021-09-13 22:49:10 -0400

    Código otimizado.
    Código do exercício 15 da lista 08 otimizado.

--- Lista08/ex015.py
+++ Lista08/ex015.py
@@ -2,13 +2,9 @@ fun = int(input('Quantos funcionários serão pagos? '))
 m = [input(f'"Pés", "mãos" e "pés e mãos" feitos pelo {i+1} funcionário (p/m/pm): ').split('/') for i in range(fun)]
 sal = [0 for i in range(fun)]
 for i in range(fun):
-    for j in range(3):
-        if j == 0:
-            m[i][j] = int(m[i][j]) * 10
-        elif j == 1:
-            m[i][j] = int(m[i][j]) * 15
-        else:
-            m[i][j] = int(m[i][j]) * 30
-        sal[i] += m[i][j]
+    m[i][0] = int(m[i][0]) * 10
+    m[i][1] = int(m[i][1]) * 15
+    m[i][2] = int(m[i][2]) * 30
+    sal[i] += m[i][0] + m[i][1] + m[i][2]
 for i in range(fun):
     print(f'Salário do {i+1}° funcionário: R${sal[i]:.2f}')
